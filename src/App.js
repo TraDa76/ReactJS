62ec43d061bd84bfeb58293a98d50cb241519c8c
Tra Da <huynhminhtri1997@gmail.com>
2021-07-26 10:29:29 +0700

    Update 26/07/2021

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Huỳnh Minh Trí 1997 in Ho Chi Minh City
+          Learn React Huỳnh Minh Trí 1997 in Binh Tan District, Ho Chi Minh City
         </a>
       </header>
     </div>
