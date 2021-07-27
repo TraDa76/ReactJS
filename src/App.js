a7c4b0c26a1052892e87d8b82ab7a415f6fd32f3
Tra Da <huynhminhtri1997@gmail.com>
2021-07-27 16:41:54 +0700

    Update 2

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Minh Tri
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Huynh Minh Tri in Binh Tan District, Ho Chi Minh City, Vietnam
-        </a>
       </header>
     </div>
   );
